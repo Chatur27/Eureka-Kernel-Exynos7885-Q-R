8cfb0d1e699847c0d710af801d63159d4ca6e368
chatur27 <jasonbright2709@gmail.com>
2021-04-06 10:08:08 +0400

    battery: add second charger_control_init
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- drivers/battery_v2/include/sec_battery.h
+++ drivers/battery_v2/include/sec_battery.h
@@ -671,6 +671,8 @@ enum {
 
 extern void select_pdo(int num);
 
+void charger_control_init(struct sec_battery_info *info);
+
 extern int adc_read(struct sec_battery_info *battery, int channel);
 extern void adc_init(struct platform_device *pdev, struct sec_battery_info *battery);
 extern void adc_exit(struct sec_battery_info *battery);
