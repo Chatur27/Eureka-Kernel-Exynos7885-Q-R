4c53054b83a54529fc05856e6221866adfc05e0e
cryptomilk <https://cryptomilk.org/>
2021-04-06 10:12:35 +0400

    battery: Fix multiplication factor for input voltage cat /sys/devices/platform/battery/power_supply/usb/voltage_max 5000000
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- drivers/battery_v2/sec_battery.c
+++ drivers/battery_v2/sec_battery.c
@@ -7365,7 +7365,7 @@ static int sec_usb_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_VOLTAGE_MAX:
 		/* V -> uV */
-		val->intval = battery->input_voltage * 1000000;
+		val->intval = battery->input_voltage * 100000;
 		return 0;
 	case POWER_SUPPLY_PROP_CURRENT_MAX:
 		/* mA -> uA */
@@ -7450,7 +7450,7 @@ static int sec_ac_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_VOLTAGE_MAX:
 		/* V -> uV */
-		val->intval = battery->input_voltage * 1000000;
+		val->intval = battery->input_voltage * 100000;
 		return 0;
 	case POWER_SUPPLY_PROP_CURRENT_MAX:
 		/* mA -> uA */
@@ -7499,7 +7499,7 @@ static int sec_wireless_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_VOLTAGE_MAX:
 		/* V -> uV */
-		val->intval = battery->input_voltage * 1000000;
+		val->intval = battery->input_voltage * 100000;
 		return 0;
 	case POWER_SUPPLY_PROP_CURRENT_MAX:
 		/* mA -> uA */
