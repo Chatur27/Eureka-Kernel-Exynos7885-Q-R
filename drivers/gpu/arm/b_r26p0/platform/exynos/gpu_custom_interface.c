a3c4a025e54069f795d6f6d3b48da707dab000c9
chatur27 <jasonbright2709@gmail.com>
2021-04-07 10:27:49 +0400

    mali/b_r26p0: Since many improvements have been done to GPU, lets fake GPU model to Mali-G72 so that some games can give the best of their performance.
    TEST: Doesn't work probably because GPU model is fetched from vendor
    
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- drivers/gpu/arm/b_r26p0/platform/exynos/gpu_custom_interface.c
+++ drivers/gpu/arm/b_r26p0/platform/exynos/gpu_custom_interface.c
@@ -2154,7 +2154,7 @@ static ssize_t show_kernel_sysfs_gpu_model(struct kobject *kobj, struct kobj_att
 		char *name;
 	} gpu_product_id_names[] = {
 		{ .id = GPU_ID2_PRODUCT_TMIX >> GPU_ID_VERSION_PRODUCT_ID_SHIFT,
-		  .name = "Mali-G71" },
+		  .name = "Mali-G72" },
 		{ .id = GPU_ID2_PRODUCT_THEX >> GPU_ID_VERSION_PRODUCT_ID_SHIFT,
 		  .name = "Mali-THEx" },
 	};
