40644b1fb3db0fc249eaae00f28c1a0d524f5f74
chatur27 <jasonbright2709@gmail.com>
2021-04-17 11:38:28 +0400

    sound/soc/codecs/tfa9896: return correct errors
    Clang warns:
    .../sound/soc/codecs/tfa9872/tfa_dsp.c:791:11: error: implicit conversion from enumeration type 'enum tfa98xx_error' to different enumeration type 'enum tfa98xx_dmem' [-Werror,-Wenum-conversion]
                            return TFA_ERROR;
                            ~~~~~~ ^~~~~~~~~
    .../sound/soc/codecs/tfa9872/tfa_dsp.c:903:10: error: implicit conversion from enumeration type 'enum tfa_error' to different enumeration type 'enum tfa98xx_error' [-Werror,-Wenum-conversion]
                    return tfa_error_bad_param;
                    ~~~~~~ ^~~~~~~~~~~~~~~~~~~
    .../sound/soc/codecs/tfa9872/tfa_dsp.c:925:10: error: implicit conversion from enumeration type 'enum tfa_error' to different enumeration type 'enum tfa98xx_error' [-Werror,-Wenum-conversion]
                    return tfa_error_bad_param;
                    ~~~~~~ ^~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- sound/soc/codecs/tfa9896/tfa_dsp.c
+++ sound/soc/codecs/tfa9896/tfa_dsp.c
@@ -854,7 +854,7 @@ tfa98xx_filter_mem(tfa98xx_handle_t dev,
 		case 0x72:
 		default:
 			/* unsupported case, possibly intermediate version */
-			return TFA_ERROR;
+			return TFA98XX_DMEM_ERR;
 			_ASSERT(0);
 		}
 	}
@@ -979,7 +979,7 @@ enum tfa98xx_error tfa98xx_set_saam_use_case(int samstream)
 
 	if (devcount < 1) {
 		pr_err("No or wrong container file loaded\n");
-		return tfa_error_bad_param;
+		return TFA98XX_ERROR_BAD_PARAMETER;
 	}
 
 	for (dev = 0; dev < devcount; dev++)
@@ -1000,7 +1000,7 @@ enum tfa98xx_error tfa98xx_set_stream_state(int stream_state)
 
 	if (devcount < 1) {
 		pr_err("No or wrong container file loaded\n");
-		return tfa_error_bad_param;
+		return TFA98XX_ERROR_BAD_PARAMETER;
 	}
 
 	for (dev = 0; dev < devcount; dev++)
